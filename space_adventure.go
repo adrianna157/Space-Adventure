445b922f05dc2ca9ef1c87a365b565a21c54a161
Adrianna <adriannaestelleguevarra@gmail.com>
2019-10-22 04:12:19 -0700

    Greeting and first input done

--- space_adventure.go
+++ space_adventure.go
@@ -1,6 +1,43 @@
-// Initial Commit
-func ()  {
-	
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	println("Welcome to The Solar System! ")
+	println("There are 9 planets to explore.")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("What is your Name?: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
+
+	fmt.Println(text, "My name is Eliza, I'm an old friend of Alex. Let's go on an adventure!")
+	text2 := ""
+	fmt.Scanln(text2)
+	fmt.Println(text2)
+
+	ln := ""
+	fmt.Sscanln("%v", ln)
+	fmt.Println(ln)
 }
 
-//2nd Test
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// )
+
+// func main() {
+
+// 	reader := bufio.newReader(os.Stdin)
+// 	var name string
+// 	println("Welcome to The Solar System! ")
+// 	println("There are 9 planets to explore.")
+// 	fmt.Println("What is your name?")
+// 	name := reader.readString("\n")
+// 	fmt.Println(name, "My name is Eliza, I'm an old friend of Alex. Let's go on an adventure!")
+// }
