6be56ed97a12a7e17db8ca57c74092dcc87ce81a
Adrianna <adriannaestelleguevarra@gmail.com>
2019-10-22 04:30:03 -0700

    Another user input

--- space_adventure.go
+++ space_adventure.go
@@ -19,25 +19,13 @@ func main() {
 	fmt.Scanln(text2)
 	fmt.Println(text2)
 
+	fmt.Println("Shall I randomly choose a planet for you? Y or N ")
+	text3, _ := reader.ReadString('\n')
+	fmt.Scanln(text3)
+	fmt.Println("name: Venus",
+		"description: It's very cloudy here!")
+
 	ln := ""
 	fmt.Sscanln("%v", ln)
 	fmt.Println(ln)
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"os"
-// )
-
-// func main() {
-
-// 	reader := bufio.newReader(os.Stdin)
-// 	var name string
-// 	println("Welcome to The Solar System! ")
-// 	println("There are 9 planets to explore.")
-// 	fmt.Println("What is your name?")
-// 	name := reader.readString("\n")
-// 	fmt.Println(name, "My name is Eliza, I'm an old friend of Alex. Let's go on an adventure!")
-// }
